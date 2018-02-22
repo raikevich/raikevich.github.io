0b6f76b7cd01e39829e5a9baee661aed2b3d4cf6
raikevich <35536334+raikevich@users.noreply.github.com>
2018-02-22 19:12:06 +0300

    Add files via upload

---
+++ Nafta_Group_180222/js/script.js
@@ -0,0 +1,105 @@
+'use strict';
+
+$(document).ready(function() {
+    if( Modernizr.touchevents ){
+        $('body').addClass('sensor');
+    }
+    if ($('[data-fancybox]').length) {
+        $('[data-fancybox]').fancybox();
+    }
+    if ($('.main_slider').length) {
+        $('.main_slider').lightSlider({
+            item: 1,
+            loop: true,
+            pager: true,
+            auto: true,
+            pause: 3000,
+            speed: 700,
+            pauseOnHover: false
+        });
+    }
+
+    if ($('.partners_slider').length) {
+        $('.partners_slider').lightSlider({
+            item: 6,
+            loop: true,
+            slideMove: 1,
+            pager: false,
+            auto: true,
+            pause: 3000,
+            speed: 700,
+            pauseOnHover: false,
+            controls:false,
+            responsive: [
+                {
+                    breakpoint: 991,
+                    settings: {
+                        item: 4,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        item: 3,
+                    }
+                },
+                {
+                    breakpoint: 575,
+                    settings: {
+                        item: 2,
+                    }
+                }
+            ]
+        });
+    }
+
+    var wp = window.innerWidth - document.documentElement.clientWidth;
+
+    if ($('.nav__click').length) {
+        $('.nav__click').on('click', function() {
+            $('.nav').addClass('active');
+            $('body').addClass('o-hidden').css('margin-right', wp + 'px');
+        });
+        $('.nav .fa-close').on('click', function() {
+            $('.nav').removeClass('active');
+            $('body').removeClass('o-hidden').css('margin-right','0px');
+        });
+    }
+
+    $('[href*="#"]').on('click', function(e){
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top }, 1000);
+        e.preventDefault();
+    });
+
+    $(window).on("load resize scroll", function(){
+        if (window.innerWidth>767) {
+            if ($(window).scrollTop() > window.innerHeight/2) {
+                $('.click_top').addClass('active');
+            } else {
+                $('.click_top').removeClass('active');
+            }
+        }
+        else {
+            $('.click_top').removeClass('active');
+        }
+    });
+
+    $('#send_mail_btn').on('click',function () {
+        var t = $('#send_mail').serialize();
+        $.ajax({
+            type: "POST",
+            url: 'mail.php',
+            data: t,
+            beforeSend: function() {
+                $('#send_mail_result').html('');
+            },
+            error: function(){
+                $('#send_mail_result').text('Ошибка сервера. Попробуйте позже');
+            },
+            success: function(data) {
+                $('#send_mail_result').html(data);
+            }
+        });
+    });
+
+});
