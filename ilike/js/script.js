a261559f48597e1b0d1a0683456831e112b015c4
raikevich <35536334+raikevich@users.noreply.github.com>
2018-06-27 15:50:29 +0300

    Add files via upload

---
+++ ilike/js/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+$(document).ready(function() {
+    if($('.my-flipster').length) {
+        $('.my-flipster').flipster({
+            loop: true,
+            scrollwheel: false,
+            buttons: 'custom',
+            buttonPrev: '<div class="flip_btn flip_btn__prev"></div>',
+            buttonNext: '<div class="flip_btn flip_btn__next"></div>'
+        });
+    }
+    if($('.sfera_slider').length) {
+        $('.sfera_slider').lightSlider({
+            item: 3,
+            loop: true,
+            slideMove: 1,
+            pager: false,
+            auto: true,
+            pause: 3000,
+            speed: 700,
+            pauseOnHover: false,
+            controls: true,
+            prevHtml: '<div class="flip_btn flip_btn__prev"></div>',
+            nextHtml: '<div class="flip_btn flip_btn__next"></div>',
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        item: 2,
+                    }
+                },
+                {
+                    breakpoint: 575,
+                    settings: {
+                        item: 1,
+                    }
+                }
+            ]
+        });
+    }
+    if($('.case_slider').length) {
+        $('.case_slider').lightSlider({
+            item: 1,
+            loop: true,
+            slideMove: 1,
+            pager: false,
+            auto: true,
+            pause: 8000,
+            speed: 700,
+            pauseOnHover: false,
+            controls: true,
+            prevHtml: '<div class="flip_btn flip_btn__prev flip_btn__black"></div>',
+            nextHtml: '<div class="flip_btn flip_btn__next flip_btn__black"></div>'
+        });
+    }
+    $('[href*="#"]').on('click', function(e){
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top }, 1000);
+        e.preventDefault();
+        $('.menu').removeClass('active');
+    });
+    $('.menu_btn').on('click', function(){
+        $('.menu').toggleClass('active');
+    });
+    $(document).on('click',function (e){
+        var menu = $('.menu');
+        if (!menu.is(e.target) && menu.has(e.target).length === 0) {
+            $('.menu').removeClass('active');
+        }
+    });
+});
