c1331326cd6d65f0b62a805d2557661eab9ee7e0
raikevich <35536334+raikevich@users.noreply.github.com>
2018-07-16 15:55:05 +0300

    Add files via upload

--- Nafta_Group/js/script.js
+++ Nafta_Group/js/script.js
@@ -53,6 +53,33 @@ $(document).ready(function() {
         });
     }
 
+    if ($('.blag_slider').length) {
+        $('.blag_slider').lightSlider({
+            item: 3,
+            loop: true,
+            slideMove: 1,
+            pager: false,
+            auto: true,
+            pause: 3000,
+            speed: 700,
+            pauseOnHover: false,
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        item: 2,
+                    }
+                },
+                {
+                    breakpoint: 575,
+                    settings: {
+                        item: 1,
+                    }
+                }
+            ]
+        });
+    }
+
     var wp = window.innerWidth - document.documentElement.clientWidth;
 
     if ($('.nav__click').length) {
@@ -83,5 +110,53 @@ $(document).ready(function() {
             $('.click_top').removeClass('active');
         }
     });
+
+    $('#send_mail_btn').on('click',function () {
+        var t = $('#send_mail').serialize();
+        $.ajax({
+            type: "POST",
+            url: 'mail.php',
+            data: t,
+            beforeSend: function() {
+                $('#send_mail_result').html('');
+            },
+            error: function(){
+                $('#send_mail_result').text('Ошибка сервера. Попробуйте позже');
+            },
+            success: function(data) {
+                $('#send_mail_result').html(data);
+            }
+        });
+    });
+
+    if ($('.ham_list').length) {
+        $('.ham_list').on('click',function () {
+            $('.service_list').addClass('active');
+        });
+    }
+
+    if ($('.ham_grid').length) {
+        $('.ham_grid').on('click',function () {
+            $('.service_list').removeClass('active');
+        });
+    }
+
+    if ($('.about_tabs').length) {
+        function aboutBottom() {
+            var tabA = $('.tabs_yearItem.active');
+            $('.tabs_bottom').css({'left':tabA.offset().left, 'top':tabA.offset().top+tabA.outerHeight(), 'width':tabA.outerWidth()});
+            var tabC = $('.tabs_contentItem.active');
+            $('.tabs_content').css({'height':tabC.outerHeight()});
+        }
+        aboutBottom();
+        $('.tabs_yearItem').on('click',function () {
+            $('.tabs_yearItem').removeClass('active');
+            $(this).addClass('active');
+            $('.tabs_contentItem').removeClass('active');
+            $('.content_'+$(this).attr('id')).addClass('active');
+            aboutBottom();
+        });
+    }
+
 
 });
